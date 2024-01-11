2ec9faa8b4e3bcf4a9263104865e6c30f90ade47
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2024-01-12 04:43:32 +0500

    assignment completed

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,3 +1,23 @@
+// import { InferType, ObjectSchema } from "yup";
+
+// export async function validate<T extends object>(
+//   schema: ObjectSchema<T>,
+//   fields: InferType<typeof schema>
+// ): Promise<InferType<typeof schema>> {
+//   try {
+//     const validatedFields = await schema.validate(fields, {
+//       abortEarly: false,
+//       stripUnknown: true,
+//     });
+//     return validatedFields;
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
+
+// The code above generates a types conflict error,
+//  and unfortunately, I couldn't resolve it. So that it why, the `validate` method below accepts `schema` and `fields` as any.
+
 import { BadRequest } from "@/utils/errors/custom-errors";
 
 async function validate(schema, fields) {
